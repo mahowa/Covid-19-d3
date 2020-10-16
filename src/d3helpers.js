a7a9cb9ba57b272b1fa1f17744ce7da0d62a18fc
Matt Howa <matt.j.howa@gmail.com>
2020-10-16 13:17:20 -0600

    Set up branch to only incluide the dist files

--- src/d3helpers.js
+++
@@ -1,6 +0,0 @@
-import * as d3 from "d3";
-
-// HacktoberFest Issue: Add JS docs (HINT: Look for examples in other files)
-export const addContainer = (id) => {
-  d3.select("#root").append("div").attr("id", id);
-};
