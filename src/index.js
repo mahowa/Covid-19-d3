d380d913c960c45e746ca113951c53c902fb4971
Raj Srinivas Jena <44380810+RajSrinivasJena@users.noreply.github.com>
2020-10-23 02:06:44 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 //  Datasets: https://github.com/nytimes/covid-19-data/
 //  Controler for initializing Charts
-import init from './data';
-import LineChart from './LineChart';
+import init from "./data";
+import LineChart from "./LineChart";
 
 const main = async () => {
+  var overlay = document.getElementById("overlay");
+
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 500);
   // Hacktober Fest Issue: create a cool loader for when data is getting pulled in
   // Get all data
   const data = await init();
-  console.log('Initialized data sets', Object.keys(data));
+  console.log("Initialized data sets", Object.keys(data));
   LineChart(data);
 };
 
