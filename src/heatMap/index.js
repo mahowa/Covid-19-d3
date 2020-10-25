62b9ce8f7f946b8bdb83a5b05bcf357b224e02b5
Emma Slight <emma.slight@icloud.com>
2020-10-26 07:04:32 +1300

    heatMap- change color for map, fix style.css merge
    Add title to map
    
    refactor code

--- src/heatMap/index.js
+++ src/heatMap/index.js
@@ -1,45 +1,117 @@
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
+import * as d3 from 'd3'
+import * as topojson from 'topojson-client'
+// import legend from './legend'
+
+// Hacktoberfest: Update this function to animate the data change instead of redrawing the chart each time
+function responsivefy(svg) {
+  // container will be the DOM element the svg is appended to
+  // we then measure the container and find its aspect ratio
+  const container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style('width'), 10),
+    height = parseInt(svg.style('height'), 10),
+    aspect = width / height
+
+  // add viewBox attribute and set its value to the initial size
+  // add preserveAspectRatio attribute to specify how to scale
+  // and call resize so that svg resizes on inital page load
+  svg
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .attr('preserveAspectRatio', 'xMinYMid')
+    .call(resize)
+
+  // add a listener so the chart will be resized when the window resizes
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on('resize.' + container.attr('id'), resize)
+
+  // this is the code that actually resizes the chart
+  // and will be called on load and in response to window resize
+  // gets the width of the container and proportionally resizes the svg to fit
+  function resize() {
+    const targetWidth = parseInt(container.style('width'))
+    svg.attr('width', targetWidth)
+    svg.attr('height', Math.round(targetWidth / aspect))
+  }
+}
 
 export default function HeatMap(data) {
-  const width =
-    window.innerWidth ||
-    document.documentElement.clientWidth ||
-    document.body.clientWidth;
+  const margin = { top: 25, right: 10, bottom: 50, left: 10 }
+  let width = 0
+  let height = 0
+  let y
+  let x
+  console.log(data)
 
-  const height = (width * 2) / 3;
-  const projection = d3.geoAlbersUsa().fitWidth();
-  const path = d3.geoPath();
-  const format = d3.format('d');
+  const calculateDimensions = () => {
+    const screenWidth = document.querySelector('#map').clientWidth
+    const chartHeight = screenWidth / 2
 
-  const color = d3.scaleSequential([0, 1500], d3.interpolateYlGn);
+    width = screenWidth - margin.left - margin.right
+    height = chartHeight - margin.top - margin.bottom
+  }
+
+  calculateDimensions()
+  const path = d3.geoPath()
+  const format = d3.format('d')
+
+  const projection = d3
+    .geoAlbersUsa()
+    .translate([width / 2, height / 2]) // translate to center of screen
+    .scale(700)
+
+  const color = d3.scaleSequential([0, 2000], d3.interpolateRdPu)
 
   const svg = d3
     .select('#map')
     .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .call(responsivefy) // this is all it takes to make the chart responsive
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-  const g = svg.append('g');
+  const g = svg.append('g')
+
+  svg
+    .append('text')
+    .attr('transform', 'translate(-70 ,0)')
+    .attr('x', width / 2)
+    .attr('y', 20)
+    .attr('class', 'title')
+    .style('fill', 'white')
+    .style('font-size', '25px')
+    .text('COVID 19 US cases per day')
 
   d3.json(
     'https://cdn.jsdelivr.net/npm/us-atlas@3.0.0/counties-albers-10m.json'
   ).then(function (us) {
-    console.log(us);
+    console.log(us)
     g.selectAll('path')
       .data(topojson.feature(us, us.objects.counties).features)
       .enter()
       .append('path')
+      .attr('d', path)
       .attr('fill', (d) => color(data.get(d.id)))
-      .attr('d', path);
 
     svg
       .append('path')
-      .datum(topojson.mesh(us, us.objects.counties, (a, b) => a !== b))
+      .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
       .attr('fill', 'none')
       .attr('stroke', 'white')
       .attr('stroke-linejoin', 'round')
-      .attr('d', path);
-  });
-  return svg.node();
+      .attr('d', path)
+
+    // let svgLegend = d3.select('body').append('svg').attr('width', 600);
+    // let defs = svgLegend.append('defs');
+    // let linearGradient = defs
+    //   .append('linearGradient')
+    //   .attr('id', 'linearGradient');
+  })
+
+  return svg.node()
+}
+const legend = () => {
+  const colorScale = d3.scaleLinear().domain([])
 }
